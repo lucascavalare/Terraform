760d419eeed82dedce5178f8f4cf3f2cef2c7604
Lucas Cavalare <lucascavalare@gmail.com>
2019-05-15 19:56:56 +0200

    Testing some arguments

--- AWS/aws_kubernetes_cluster.tf
+++ AWS/aws_kubernetes_cluster.tf
@@ -2,22 +2,25 @@
     region = "eu-west-3"
   }
 
+/*
   resource "aws_subnet" "kubernetes" {
     vpc_id = "${aws_vpc.kubernetes.id}"
     availability_zone = "eu-west-3a"
   }
+*/
 
   resource "aws_instance" "kube" {
     count = 3
     ami = "ami-0eeeef929db40543c" # Ubuntu 16.04 Version to meet test req.
     instance_type = "t2.micro"
     
-    subnet_id = "${aws_subnet.kubernetes.id}"
+    #subnet_id = "${aws_subnet.kubernetes.id}"
     associate_public_ip_address = true
     
     availability_zone = "eu-west-3a"
   } 
 
+/*
   resource "aws_elb" "kubernetes_api" {
     name = "kube-api"
     instances = ["${aws_instance.controller.*.id}"]
@@ -41,4 +44,4 @@
       interval = 30
     }
   }
-  
+*/  
