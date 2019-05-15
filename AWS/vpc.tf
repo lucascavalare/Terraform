7edebd9c515844869b990007513bc27889de14f6
Lucas Cavalare <lucascavalare@gmail.com>
2019-05-15 21:36:37 +0200

    Creating VPC, GW, and Routing

---
+++ AWS/vpc.tf
@@ -0,0 +1,29 @@
+# VPC Resources
+
+# Creating VPC 
+resource "aws_vpc" "test" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Creating Subnet
+resrouce "aws_subnet" "test" {
+  availability_zone = "eu-west-3a"
+  cidr_block = "10.0.${count.index}.0/24"
+  vpc_id = "${aws_vpc.test.id}"
+}
+
+# Creating Internet Gateway 
+resource "aws_internet_gateway" "test" {
+  vpc_id = "${aws_vpc.test.id}"
+  
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.test.id}"
+  }
+}
+
+# Creating Route Table Association
+resource "aws_route_table_association" "test" {
+  subnet_id = "${aws_subnet.test.*.id[count.index]}"  
+  route_table_id = "${aws_route_table.demo.id}"
+}
