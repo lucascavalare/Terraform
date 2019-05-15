48955b10987155d9c5e314625a8cf69733aecff2
Lucas Cavalare <lucascavalare@gmail.com>
2019-05-15 20:17:48 +0200

    Update aws_kubernetes_cluster.tf

--- AWS/aws_kubernetes_cluster.tf
+++ AWS/aws_kubernetes_cluster.tf
@@ -1,7 +1,9 @@
   provider "aws" {
     region = "eu-west-3"
   }
-  
+
+  data "aws_availability_zones" "available" {}
+
   resource "aws_vpc" "test" {
     cidr_block = "10.0.0.0/16"
     tags = "${
