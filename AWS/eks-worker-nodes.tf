eb51f247ed4e34f65b1813b96268f8f7fb85ce05
Lucas Cavalare <lucascavalare@gmail.com>
2019-05-16 01:09:10 +0200

    Update eks-worker-nodes.tf

--- AWS/eks-worker-nodes.tf
+++ AWS/eks-worker-nodes.tf
@@ -1,5 +1,44 @@
 # EKS Worker Nodes 
 
+resource "aws_iam_role" "test-node" {
+  name = "terraform-eks-test-node"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "test-node-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = "${aws_iam_role.test-node.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "demo-node-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = "${aws_iam_role.test-node.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "test-node-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = "${aws_iam_role.test-node.name}"
+}
+
+resource "aws_iam_instance_profile" "demo-node" {
+  name = "terraform-eks-demo"
+  role = "${aws_iam_role.demo-node.name}"
+}
+
 # EC2 Security Group to network traffic
 resource "aws_security_group" "test-node" {
   name = "terraform-eks-test"
