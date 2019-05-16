fec3b3082bcb0b901c1d0064401767325f6980e1
Lucas Cavalare <lucascavalare@gmail.com>
2019-05-16 17:31:55 +0200

    Update subnet.tf

--- Azure/subnet.tf
+++ Azure/subnet.tf
@@ -1,6 +1,6 @@
-resource "azurerm_subnet" "mytestsubnet" {
+resource "azurerm_subnet" "testsubnet" {
     name                 = "mysubnet"
     resource_group_name  = "${azurerm_resource_group.rg.name}"
-    virtual_network_name = "${azurerm_virtual_network.mytestnetwork.name}"
+    virtual_network_name = "${azurerm_virtual_network.testnetwork.name}"
     address_prefix       = "10.0.2.0/24"
 }
