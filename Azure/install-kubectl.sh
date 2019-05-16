d905e7eb66083fa8be286cc6cdd0c9690ceff0d5
Lucas Cavalare <lucascavalare@gmail.com>
2019-05-17 00:10:53 +0200

    elevating:privileges

--- Azure/install-kubectl.sh
+++ Azure/install-kubectl.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
-sudo -i
-
 echo "Updating..."
-apt-get update
-apt-get install -y apt-transport-https
+sudo apt-get update
+sudo apt-get install -y apt-transport-https
 
 # Install VirtualBox & Extension Packs
-apt-get install -y virtualbox virtualbox-ext-pack
+sudo apt-get install -y virtualbox virtualbox-ext-pack
 
 # Getting Keys & Add Kubernetes REPO to APT source list & Install kubectl to interact with Cluster
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 touch /etc/apt/sources.list.d/kubernetes.list 
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 echo "Updating..."
-apt-get update
-apt-get install -y kubectl
+sudo apt-get update
+sudo apt-get install -y kubectl
 
 # Install Minikube to run single node Kubernetes Cluster on VM
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.28.2/minikube-linux-amd64
-chmod +x minikube && sudo mv minikube /usr/local/bin/
+sudo chmod +x minikube && sudo mv minikube /usr/local/bin/
 
 # Start Minikube & Kubectl to show API Versions 
-minikube start
-kubectl api-versions
+sudo minikube start
+sudo kubectl api-versions
