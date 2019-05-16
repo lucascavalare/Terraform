43d2b035d7c7fbcc8edc4dfba05ded3707bcaacb
Lucas Cavalare <lucascavalare@gmail.com>
2019-05-16 19:32:14 +0200

    Changing region:westeu; name net

--- Azure/virtual-network.tf
+++ Azure/virtual-network.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "testnetwork" {
-    name                = "mytestnet"
+    name                = "myTestNet"
     address_space       = ["10.0.0.0/16"]
-    location            = "eastus"
+    location            = "westeurope"
     resource_group_name = "${azurerm_resource_group.rg.name}"
 
     tags {
