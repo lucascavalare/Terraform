03b7de0eed33bc3819181f8c9665c298ac921c26
Lucas Cavalare <lucascavalare@gmail.com>
2019-05-16 23:48:01 +0200

    env:virtual_network

--- Azure/vm.tf
+++ Azure/vm.tf
@@ -43,6 +43,7 @@ resource "azurerm_virtual_machine" "testvm" {
         
         environment {
           resource_group_name = "${azurerm_resource_group_name.rg.name}"
+          virtual_network_name = "${azurerm_virtual_network.testnetwork.name}"
         }
     }
 }
