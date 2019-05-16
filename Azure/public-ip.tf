6f8a3b129848765fa8c52d5018e79b69d2ca33ad
Lucas Cavalare <lucascavalare@gmail.com>
2019-05-16 19:49:15 +0200

    Changing region:westeu

--- Azure/public-ip.tf
+++ Azure/public-ip.tf
@@ -1,6 +1,6 @@
 resource "azurerm_public_ip" "testpublicip" {
     name                         = "myPublicIP"
-    location                     = "eastus"
+    location                     = "westeurope"
     resource_group_name          = "${azurerm_resource_group.rg.name}"
     allocation_method            = "Dynamic"
 
