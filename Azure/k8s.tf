74ea29c8489c143d57c3da75103fbaf8b58d7c61
Lucas Cavalare <lucascavalare@gmail.com>
2019-05-16 16:45:37 +0200

    Update k8s.tf

--- Azure/k8s.tf
+++ Azure/k8s.tf
@@ -1,8 +1,13 @@
+/*
 # Creating Azure Resource group.
 resource "azurerm_resource_group" "k8s" {
   name     = "${var.resource_group_name}"
   location = "${var.location}"
 }
-
-
+*/
+  
+resource "azurerm_resource_group" "rg" {
+  name = "testResourceGroup"
+  location = "westeu"
+}
  
