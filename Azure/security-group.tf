c3448fa95fa88165e090494ec8df37bf380329d2
Lucas Cavalare <lucascavalare@gmail.com>
2019-05-16 17:36:26 +0200

    Adding region:westeu

--- Azure/security-group.tf
+++ Azure/security-group.tf
@@ -1,6 +1,6 @@
 resource "azurerm_network_security_group" "testsg" {
     name                = "myNetworkSecurityGroup"
-    location            = "easteurope"
+    location            = "westeurope"
     resource_group_name = "${azurerm_resource_group.rg.name}"
     
     security_rule {
