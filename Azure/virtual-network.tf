eff16b903d941ebca5b081fdd8fd4505871ec00d
Lucas Cavalare <lucascavalare@gmail.com>
2019-05-16 17:29:37 +0200

    Create virtual-network.tf

---
+++ Azure/virtual-network.tf
@@ -0,0 +1,10 @@
+resource "azurerm_virtual_network" "testnetwork" {
+    name                = "mytestnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "eastus"
+    resource_group_name = "${azurerm_resource_group.rg.name}"
+
+    tags {
+        environment = "terraform"
+    }
+}
