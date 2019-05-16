2f62b637c3aeea59a391d673a6510416f20f2b70
Lucas Cavalare <lucascavalare@gmail.com>
2019-05-16 17:37:52 +0200

    Create storage.tf

---
+++ Azure/storage.tf
@@ -0,0 +1,10 @@
+# Creating storage to store VM boot options
+
+resource "random_id" "randomId" {
+    keepers = {
+        # Generate a new ID only when a new resource group is defined
+        resource_group = "${azurerm_resource_group.rg.name}"
+    }
+    
+    byte_length = 8
+}
