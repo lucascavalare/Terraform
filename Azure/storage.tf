3fd09f07b2a45e9bd9bfd8ba4e5e50d9e5103b25
Lucas Cavalare <lucascavalare@gmail.com>
2019-05-16 17:40:10 +0200

    Update storage.tf

--- Azure/storage.tf
+++ Azure/storage.tf
@@ -1,5 +1,4 @@
-# Creating storage to store VM boot options
-
+# Creating random ID
 resource "random_id" "randomId" {
     keepers = {
         # Generate a new ID only when a new resource group is defined
@@ -7,4 +6,17 @@ resource "random_id" "randomId" {
     }
     
     byte_length = 8
+}
+
+# Creating storage account to store VM boot options
+resource "azurerm_storage_account" "teststorageaccount" {
+    name                = "diag${random_id.randomId.hex}"
+    resource_group_name = "${azurerm_resource_group.rg.name}"
+    location            = "westeurope"
+    account_replication_type = "LRS"
+    account_tier = "Standard"
+
+    tags {
+        environment = "terraform"
+    }
 }
