f166b8c34635b148772d8da39d14f0a4c7bbe429
Lucas Cavalare <lucascavalare@gmail.com>
2019-05-18 00:15:09 +0200

    Adding:SG:HTTP

--- Azure/security-group.tf
+++ Azure/security-group.tf
@@ -14,7 +14,18 @@ resource "azurerm_network_security_group" "testsg" {
         source_address_prefix      = "*"
         destination_address_prefix = "*"
     }
-
+    
+    security_rule {
+        name                       = "HTTP"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "8080"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
     tags {
         environment = "terraform"
     }
