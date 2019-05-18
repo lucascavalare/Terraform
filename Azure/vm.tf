cd8a25b1b5559f60f1710bee83d8082939dfaea4
Lucas Cavalare <lucascavalare@gmail.com>
2019-05-18 14:13:09 +0200

    Changing storage:Standard_SSD

--- Azure/vm.tf
+++ Azure/vm.tf
@@ -10,7 +10,7 @@ resource "azurerm_virtual_machine" "testvm" {
         name              = "myOsDisk"
         caching           = "ReadWrite"
         create_option     = "FromImage"
-        managed_disk_type = "Premium_LRS"
+        managed_disk_type = "Standard_SSD"
     }
 
     storage_image_reference {
