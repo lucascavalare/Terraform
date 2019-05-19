11fe59e4c945384d7128bff42e443c7551fdb6bf
Lucas Cavalare <lucascavalare@gmail.com>
2019-05-19 17:23:10 +0200

    adding:data

--- Azure/vm.tf
+++ Azure/vm.tf
@@ -51,3 +51,9 @@ resource "azurerm_virtual_machine" "testvm" {
         }
     }
 }
+
+data "azurerm_public_ip" "test" {
+  name                = "${azurerm_public_ip.pip.name}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  depends_on          = ["azurerm_virtual_machine.testvm"]
+}
