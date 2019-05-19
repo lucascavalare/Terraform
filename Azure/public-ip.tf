1a97ab40f58b15c509c41406b9e8669a2c517a17
Lucas Cavalare <lucascavalare@gmail.com>
2019-05-19 17:32:28 +0200

    Update public-ip.tf

--- Azure/public-ip.tf
+++ Azure/public-ip.tf
@@ -4,7 +4,7 @@ resource "azurerm_public_ip" "testpublicip" {
     location                     = "westeurope"
     resource_group_name          = "${azurerm_resource_group.rg.name}"
     allocation_method            = "Dynamic"
-    depends_on                   = ["azurerm_virtual_machine.testvm"]
+    #depends_on                   = ["azurerm_virtual_machine.testvm"]
     
     tags {
         environment = "terraform"
