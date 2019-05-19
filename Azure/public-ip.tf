99d6947bf4908f14ffc5e8e50d30ca8c60d4c7e0
Lucas Cavalare <lucascavalare@gmail.com>
2019-05-19 14:11:33 +0200

    updating:count.index

--- Azure/public-ip.tf
+++ Azure/public-ip.tf
@@ -1,5 +1,5 @@
 resource "azurerm_public_ip" "testpublicip" {
-    name                         = "myPublicIP"
+    name                         = "myPublicIP${count.index}"
     location                     = "westeurope"
     resource_group_name          = "${azurerm_resource_group.rg.name}"
     allocation_method            = "Dynamic"
