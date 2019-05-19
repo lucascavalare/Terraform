2e94ffd804f1d9ddc8f626ce8b03a288d865b253
Lucas Cavalare <lucascavalare@gmail.com>
2019-05-19 17:06:25 +0200

    adjusting:param allocation_method

--- Azure/public-ip.tf
+++ Azure/public-ip.tf
@@ -3,7 +3,7 @@ resource "azurerm_public_ip" "testpublicip" {
     name                         = "myPublicIP${count.index}"
     location                     = "westeurope"
     resource_group_name          = "${azurerm_resource_group.rg.name}"
-    allocation_method            = "Dynamic"
+    allocation_method            = "dynamic"
     #depends_on                   = ["azurerm_virtual_machine.testvm"]
     
     tags {
