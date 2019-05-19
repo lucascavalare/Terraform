eb2807b9dcfc280e663a33a767eff4cd72f1dbb1
Lucas Cavalare <lucascavalare@gmail.com>
2019-05-19 17:07:12 +0200

    commenting:count

--- Azure/public-ip.tf
+++ Azure/public-ip.tf
@@ -1,9 +1,9 @@
 resource "azurerm_public_ip" "testpublicip" {
-    count                        = 3
+    #count                        = 3
     name                         = "myPublicIP${count.index}"
     location                     = "westeurope"
     resource_group_name          = "${azurerm_resource_group.rg.name}"
-    allocation_method            = "dynamic"
+    allocation_method            = "Dynamic"
     #depends_on                   = ["azurerm_virtual_machine.testvm"]
     
     tags {
