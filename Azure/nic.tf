a4a59f509cdc7d3aee0a65666287e1bb53358da0
Lucas Cavalare <lucascavalare@gmail.com>
2019-05-19 17:36:35 +0200

    commenting:count

--- Azure/nic.tf
+++ Azure/nic.tf
@@ -1,12 +1,12 @@
 resource "azurerm_network_interface" "testnic" {
-    count               = 3
+    #count               = 3
     name                = "myNIC${count.index}"
     location            = "westeurope"
     resource_group_name = "${azurerm_resource_group.rg.name}"
     network_security_group_id = "${azurerm_network_security_group.testsg.id}"
 
     ip_configuration {
-        count                         = 3
+        #count                         = 3
         name                          = "myNicConfiguration${count.index}"
         subnet_id                     = "${azurerm_subnet.testsubnet.id}"
         private_ip_address_allocation = "Dynamic"
