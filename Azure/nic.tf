b6ea0ba64f878fbe2af878a581940d5f076e3e68
Lucas Cavalare <lucascavalare@gmail.com>
2019-05-19 14:08:38 +0200

    adding:multiple NIC

--- Azure/nic.tf
+++ Azure/nic.tf
@@ -1,11 +1,12 @@
 resource "azurerm_network_interface" "testnic" {
-    name                = "myNIC"
+    count               = 3
+    name                = "myNIC${count.index}"
     location            = "westeurope"
     resource_group_name = "${azurerm_resource_group.rg.name}"
     network_security_group_id = "${azurerm_network_security_group.testsg.id}"
 
     ip_configuration {
-        name                          = "myNicConfiguration"
+        name                          = "myNicConfiguration${count.index}"
         subnet_id                     = "${azurerm_subnet.testsubnet.id}"
         private_ip_address_allocation = "Dynamic"
         public_ip_address_id          = "${azurerm_public_ip.testpublicip.id}"
