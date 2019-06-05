95c27a358ab77115ac605814cce908a7954d620b
Lucas Cavalare <lucascavalare@gmail.com>
2019-06-05 22:36:14 +0200

    adding: template file

--- aws/aws_staging/main.tf
+++ aws/aws_staging/main.tf
@@ -1,9 +1,9 @@
-# Setting up Provider
+# Setting up Provider.
 provider "aws" { 
   region = "eu-west-3"
 }
 
-# Creating the S3 Bucket to store Terraform State
+# Creating the S3 Bucket to store Terraform State.
 resource "aws_s3_bucket" "terraform_state" { 
   bucket = "terraform-up-and-running-state-store"
  
@@ -16,7 +16,17 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 }
 
-# Print out ARN of your Bucket S3
+# Print out ARN of your Bucket S3.
 output "s3_bucket_arn" {
   value = "${aws_s3_bucket.terraform_state.arn}"
 } 
+
+# Template file data source to get MySQL info.
+data "template_file" "user_data" {
+  template = "${file("user-data.sh")}"
+  vars {
+    server_port = "${var.server_port}"
+    db_address = "${data.terraform_remote_state.db.address}"
+    db_port = "${data.terraform_remote_state.db.port}"
+  }
+}
