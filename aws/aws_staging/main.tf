a2498ef3a742f7cd2fd3e0c3ce2238b66704fa4d
Lucas Cavalare <lucascavalare@gmail.com>
2019-06-11 22:16:40 +0200

    adding: vars to web server cluster

--- aws/aws_staging/main.tf
+++ aws/aws_staging/main.tf
@@ -7,3 +7,11 @@ provider "aws" {
 module "webserver_cluster" {
   source = "../modules"
 }
+
+# Input vars to web server.
+module "webserver_cluster" {
+source = "../modules"
+      cluster_name = "webservers-stageing"
+      db_remote_state_bucket = "(terraform_state)"
+      db_remote_state_key = "aws_staging/data-stores/mysql/terraform.tfstate"
+}
