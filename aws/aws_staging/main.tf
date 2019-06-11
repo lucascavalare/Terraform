753798fe58376b72a527c7fd6c78e4de81d9653e
Lucas Cavalare <lucascavalare@gmail.com>
2019-06-11 22:36:35 +0200

    setting: type, min and max_size instances

--- aws/aws_staging/main.tf
+++ aws/aws_staging/main.tf
@@ -5,8 +5,12 @@ provider "aws" {
 
 # Input vars to web server as module contents.
 module "webserver_cluster" {
-source = "../modules"
-      cluster_name = "webservers-stageing"
-      db_remote_state_bucket = "(terraform_state)"
-      db_remote_state_key = "aws_staging/data-stores/mysql/terraform.tfstate"
+  source = "../modules"
+  cluster_name = "webservers-stageing"
+  db_remote_state_bucket = "(terraform_state)"
+  db_remote_state_key = "aws_staging/data-stores/mysql/terraform.tfstate"
+  
+  instance_type = "t2.micro"
+  min_size = 2
+  max_size = 2
 }
