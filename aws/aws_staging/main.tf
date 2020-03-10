36d4e35344e7b012593c2213bad2b1e20ca13a19
Lucas Cavalare <lucascavalare@gmail.com>
2020-03-10 21:10:52 +0000

    Changing cluster_name = " "

--- aws/aws_staging/main.tf
+++ aws/aws_staging/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 # Input vars to web server as module contents.
 module "webserver_cluster" {
   source = "../modules"
-  cluster_name = "webservers-stageing"
+  cluster_name = "webservers-staging"
   db_remote_state_bucket = "(terraform_state)"
   db_remote_state_key = "aws_staging/data-stores/mysql/terraform.tfstate"
   
